b5a8ada91a0187d307b951e5b205c9c3e9799fa8
hendrixmar <hendrikuwabara@gmail.com>
2020-06-12 23:12:01 -0500

    first commit

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import './BusinessList.css';
+import  Bussines from '../Business/Business'
+
+class BusinessList extends React.Component{
+    render(){
+        return (<div class="BusinessList">
+            <Bussines />
+            <Bussines />
+            <Bussines />
+            <Bussines />
+            <Bussines />
+            <Bussines />
+            
+            </div>)
+
+    }
+}
+export default  BusinessList;
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import './SearchBar.css'
+
+const sortByOptions = {
+    'Best Match': 'best_match',
+    'Highest Rated': 'rating',
+    'Most Reviewed': 'review_count'
+  }
+
+/* 
+ Client ID
+
+hZ5nJEz1hLT6CfLgIczI4A
+API Key
+
+c8qhziAQNNpFhArSbb_XyGKxfAE9r2I6QZX6i4UDeH6s8_n5KGyHcEZZZGlK-ljGbaZZnXoHZuAgmG_3W_n9EaA1yCZp9YNn3MZLZNe1aXcbH-SrS8HTbNrTs7PiXnYx
+
+*/
+
+class SearchBar extends React.Component{
+    render(){
+
+    }
+
+    renderSortByOptions(){
+        return  Object.keys(sortByOptions).map(sortByOption => {
+            let sortByOptionValue = sortByOptions[sortByOption];
+            return <li key={sortByOptionValue}> sortByOption </li>;
+        });
+    }
+}
